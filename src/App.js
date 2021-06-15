7b8894c8530ce0b22b9e097ba6b744271b8b715a
Rudrak'sPC <rudrakpatracodes@gmail.com>
2021-06-15 10:41:13 +0530

    added toolbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Toolbar from "./components/Toolbar";
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    window.onload = () => {
+      console.log("initialize paper and tools");
+    };
+    this.state = {
+      // tools=[new Pencil(),new Polygon(),new Eraser(),new Shadow()]
+      tools: [
+        {
+          config: {
+            name: "sketch",
+            modifiers: {
+              color: ["color", "red"],
+              width: ["width", 2],
+              flow: ["flow", "smooth"], //line//normal//mouse friendly
+            },
+          },
+        },
+        {
+          config: {
+            name: "shape",
+            modifiers: {
+              color: ["color", "red"],
+              edgeSmoothness: ["edge smoothness", "auto"], //manual//none
+            },
+          },
+        },
+        {
+          config: {
+            name: "shadow",
+            modifiers: {
+              strength: ["strength", "50"], //0 to 100
+              editMode: ["edit mode", "simple"], //advanced
+              blur: ["blur", "50"],
+            },
+          },
+        },
+        {
+          config: {
+            name: "blend",
+            modifiers: {
+              strength: ["strength", "50"],
+            },
+          },
+        },
+      ],
+      activeTool: null,
+    };
+  }
+  setActiveTool(tool) {
+    this.setState({ activeTool: tool });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="canvasContainer">
+          <canvas id="project" resize="true"></canvas>
+        </div>
+        <Toolbar
+          tools={this.state.tools}
+          setActiveTool={this.setActiveTool.bind(this)}
+          activeTool={this.state.activeTool}
+        />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/Toolbar.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./Toolbar.css";
+class Toolbar extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+  //   }
+  renderModifiers() {
+    return (
+      <div className="modifiersList">
+        <div className="container">
+          {this.props.activeTool
+            ? Object.entries(this.props.activeTool.config.modifiers).map(
+                ([, [type, data]], i) => {
+                  return (
+                    <div
+                      key={i}
+                      className="modifier color"
+                      onClick={(event) => {
+                        console.log(`${type} : ${data} clicked`);
+                      }}
+                    >
+                      {`${type} : ${data}`}
+                    </div>
+                  );
+                }
+              )
+            : true}
+        </div>
+      </div>
+    );
+  }
+  setActiveTool(tool) {
+    this.props.setActiveTool(tool);
+  }
+  renderTools() {
+    return (
+      <div className="toolsList">
+        {this.props.tools.map((tool, i) => {
+          return (
+            <div
+              key={i}
+              className={`tool ${this.props.activeTool === tool && "active"}`}
+              onClick={() => this.setActiveTool(tool)}
+            >
+              {tool.config.name}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+  render() {
+    return (
+      <div className="toolsBar overlay">
+        <div className="content">
+          {this.renderModifiers()}
+          {this.renderTools()}
+        </div>
+      </div>
+    );
+  }
+}
+export default Toolbar;
